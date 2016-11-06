5505ee8c5b5c028a8aae822a0b1ef97bce339ac0
JCarlos Campos <solrak.cj@gmail.com>
2016-11-06 14:43:36 -0400

    estado inicial del proyecto

---
+++ src/principal/Main.java
@@ -0,0 +1,11 @@
+package principal;
+
+/**
+ * Created by JCarlos on 06/11/2016.
+ */
+public class Main {
+    public static void main(String[] arg){
+        System.out.println("pueba de una aplicacion en java con intelliJ idea");
+         
+    }
+}
